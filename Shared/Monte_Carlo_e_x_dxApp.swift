a962a752be38b42387b0a6cdfad1a50f97c5a8f1
Katelyn Lydeen <klydeen@hawk.iit.edu>
2022-02-04 15:42:47 -0600

    Initial Commit

---
+++ Shared/Monte_Carlo_e_x_dxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Monte_Carlo_e_x_dxApp.swift
+//  Shared
+//
+//  Created by Katelyn Lydeen on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Monte_Carlo_e_x_dxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
